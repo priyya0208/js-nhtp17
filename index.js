867058f2efba588fd2b1ecec5a03f3bf2ec57735
priyya0208 <priyya.0208@gmail.com>
2020-03-02 15:01:37 +0530

    1 file modified

--- index.js
+++ index.js
@@ -23,11 +23,37 @@ const acctData =[
   }
 ];
 
-const balance =[
-  {
+const balance ={
     "AAA - 1234": 4593.22,
     "AAA - 9921": 0,
     "AAA - 5231": 232142.5,
     "AAA - 8191": 4344
+};
+
+function func(param) {
+  var accNumber = [];
+  if(param == "user") {
+    acctData.forEach(function(item){
+    if(accNumber. indexOf(item.user) == -1){
+      accNumber.push(item.user);}
+    });
+  };
+  if(param == "sort_by(acctNum)"){
+    acctData.forEach(function(item){
+    if(accNumber. indexOf(item.acctNum) == -1){
+      accNumber.push(item.acctNum);}
+    });
   }
-];
+  if(param == "sort_by(balance)"){
+    for (let value of Object.values(balance)) {
+      accNumber.push(value);
+    }
+
+
+  }
+
+  return console.log(accNumber);
+};
+func("user");
+func("sort_by(acctNum)");
+func("sort_by(balance)");
